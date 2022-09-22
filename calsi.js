5204916fa8cbca5fb8a15bfb67c3450cd8c90bf6
vinay <vinayvarma2545@gmail.com>
2022-09-22 18:51:46 +0530

    calsi

---
+++ calsi.js
@@ -0,0 +1,19 @@
+const operator = ("+");
+const no1 =5;
+const no2 =1;
+
+let result;
+
+if(operator=='+'){
+    result = no1+no2
+}
+
+else if(operator=='-'){
+    result = no1-no2
+}
+
+else if(operator=='*'){
+    result = no1*no2
+}
+
+console.log(`${result}`)
